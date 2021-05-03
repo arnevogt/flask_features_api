303aa26f2dfd6ad40639a354c82ac4bd4343a871
Arne Vogt <a.vogt@52north.org>
2021-05-03 16:39:52 +0200

    advance capabilities controller

--- openapi_server/controllers/capabilities_controller.py
+++ openapi_server/controllers/capabilities_controller.py
@@ -7,6 +7,9 @@ from openapi_server.models.conf_classes import ConfClasses  # noqa: E501
 from openapi_server.models.exception import Exception  # noqa: E501
 from openapi_server.models.landing_page import LandingPage  # noqa: E501
 from openapi_server.models.link import Link  # noqa: E501
+from openapi_server.models.extent import Extent
+from openapi_server.models.extent_spatial import ExtentSpatial
+from openapi_server.models.extent_temporal import ExtentTemporal
 from openapi_server import util
 from flask import request
 
@@ -21,10 +24,7 @@ def describe_collection(collection_id):  # noqa: E501
 
     :rtype: Collection
     """
-    selfLink = Link(href=request.url, rel="self")
-    itemsLink = Link(href=request.url + "/items", rel="items")
-    c = Collection(id=collection_id, description="welcome to " +
-                   collection_id + " collection", links=[selfLink, itemsLink])
+    c = getVegetationCollection(request.url, request.url + "/items")
 
     return c
 
@@ -37,10 +37,10 @@ def get_collections():  # noqa: E501
 
     :rtype: Collections
     """
-    collectionLink1 = Link(href=request.url + "/helloworld", rel="collection")
-    selfLink = Link(href=request.url, rel="self")
-    colls = Collections(links=[selfLink, collectionLink1])
-
+    selfLink = Link(href=request.url, rel="self",
+                    title="describition of all collections in json", type="application/json")
+    colls = Collections(links=[selfLink], collections=[getVegetationCollection(
+        request.url + "/vegetation", request.url + "/vegetation/items")])
     return colls
 
 
@@ -63,9 +63,25 @@ def get_landing_page():  # noqa: E501
 
     :rtype: LandingPage
     """
-    collectionsLink = Link(request.url + "/collections", rel="collections")
-    selfLink = Link(href=request.url, rel="self")
+    collectionsLink = Link(request.url + "collections", rel="data",
+                           title="information about the feature collections")
+    selfLink = Link(href=request.url, rel="self",
+                    type="application/json", title="landing page as json")
     lp = LandingPage("TB-17 Experiments API Python Server",
                      "this is a dummy server", [selfLink, collectionsLink])
 
     return lp
+
+
+def getVegetationCollection(selfUrl, itemsUrl):
+    selfLink = Link(href=selfUrl, rel="self",
+                    title="collection describtion as json", type="application/json")
+    itemsLink = Link(href=itemsUrl, rel="items",
+                     title="vegetation", type="application/geo+json")
+    extentSp = ExtentSpatial(bbox=[[7.01, 50.63, 7.22, 50.78]])
+    extentTemp = ExtentTemporal(interval=[["2010-02-15T12:34:56Z", None]])
+    extent = Extent(spatial=extentSp, temporal=extentTemp)
+    c = Collection(id="vegetation", description="this collection contains vegetation data", links=[
+                   selfLink, itemsLink], extent=extent, title="vegetation")
+
+    return c
